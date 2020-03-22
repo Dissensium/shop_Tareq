df01d73228e48a029565d09c0be89c02f9cca790
ZegganiD <61799234+ZegganiD@users.noreply.github.com>
2020-03-22 18:54:27 +0100

    change 6
    change 6

--- web/Controllers/loginController.php
+++ web/Controllers/loginController.php
@@ -3,8 +3,8 @@ session_start();
 include_once "ClassDbBroker.php";
 include_once "ClassConfig.php";
 
-define("CLIENT","client");
-define("ADMIN","admin");
+define("CLIENT", "client");
+define("ADMIN", "admin");
 
 // On démarre la session AVANT d'écrire du code HTML
 
@@ -15,7 +15,7 @@ define("ADMIN","admin");
 $salt = ClassConfig::getInstance()->getSalt();
 $email = htmlspecialchars($_POST['username']);
 $password = htmlspecialchars($_POST['password']);
-$password = sha1($salt.$password);
+$password = sha1($salt . $password);
 
 
 
@@ -24,26 +24,21 @@ $password = sha1($salt.$password);
 
 
 
-   $myClassDbBroker = ClassDbBroker::getinstance();
-    $myClassDbBroker->getUserRole($email,$password);         
+$myClassDbBroker = ClassDbBroker::getinstance();
+$myClassDbBroker->getUserRole($email, $password);
 
-   if ($myClassDbBroker->getUserRole($email,$password) ){
-    $_SESSION['username'] = $email;
-    $_SESSION['password'] = $password;
-    echo "je suis loger";
-
-   }else{
-    echo "je suis loger";
+$roleST = $myClassDbBroker->getUserRole($email, $password);
+if ($roleSt == ADMIN) {
+   $_SESSION['username'] = $email;
+   $_SESSION['password'] = $password;
+   $_SESSION['role'] = ADMIN;
+} else {
+   if ($roleSt == CLIENT) {
+      $_SESSION['username'] = $email;
+      $_SESSION['password'] = $password;
+      $_SESSION['role'] = CLIENT;
    }
-
-         if ( $roleSt==ADMIN){
-            $_SESSION['username']=$email;
-            $_SESSION['password']=$password;
-            $_SESSION['role']=ADMIN;
-         }else{
-            echo "Normale <br> \n";            
-         }
-      }
+}
 
 
   
